b98c1c847891b8ddab7a0235a0aaee4b22d43dc1
srujants <srujansathish964@gmail.com>
2026-04-29 19:26:58 +0530

    fix: resolve merge conflicts with final day6 code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import { BrowserRouter,Routes,Route,Navigate } from "react-router-dom"
 import Login from "./pages/Login"
 import Dashboard from "./pages/Dashboard"
 import Incidents from "./pages/Incidents"
+import CreateIncident from "./pages/CreateIncident"
 import IncidentDetail from "./pages/IncidentDetail"
 import Layout from "./components/Layout"
 import ProtectedRoute from "./routes/ProtectedRoute"
-import CreateIncident from "./pages/CreateIncident"
 
 export default function App(){
  return(
@@ -16,9 +16,9 @@ export default function App(){
 
     <Route element={<ProtectedRoute/>}>
      <Route element={<Layout/>}>
-     <Route path="/create" element={<CreateIncident/>} />
       <Route path="/dashboard" element={<Dashboard/>} />
       <Route path="/incidents" element={<Incidents/>} />
+      <Route path="/create" element={<CreateIncident/>} />
       <Route path="/incident/:id" element={<IncidentDetail/>} />
      </Route>
     </Route>
